7232be23d168cc64a95c352d0eef0f0eceabf46e
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-07-25 14:32:18 +0700

    Create total.java

---
+++ total.java
@@ -0,0 +1,11 @@
+public class total {
+    public static void main(String[] args) {
+        int b, c;
+         b = 62;
+         c = 99;
+        int total = b + c;
+        System.out.println("EnterNumber 62 + 99 :" +total); 
+    }
+
+    }
+
