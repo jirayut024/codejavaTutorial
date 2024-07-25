ea4c8b7739d6a03175d215caa449cc2dab4e69c0
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-07-25 14:19:43 +0700

    Create Greatfun.java

---
+++ Greatfun.java
@@ -0,0 +1,7 @@
+public class Greatfun {
+    public static void main(String[] args) {
+        System.out.print("Program is ");
+        System.out.println("Great fun");
+        System.out.print("These ane our top sellers: ");
+    }
+}
