35ebd9bc4210e85ff08052a1c1cf782c147b1579
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-07-26 14:40:32 +0700

    Create Circlearea.java

---
+++ Circlearea.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Circlearea {
+    public static void main(String[] args) {
+        double radius;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the radius : ");
+        radius = in.nextDouble();
+        if(radius < 0)
+        System.out.println("Enter: Radius must be >= 0"); 
+        else{
+            double area = 3.141 * radius * radius;
+            System.out.println("Teh area is " +area); 
+        }
+            
+        
+    }
+}
