7de433c8b57e33d4901da000830dddb2d0d6f123
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-07-26 14:47:11 +0700

    Create ifAct.java

---
+++ ifAct.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ifAct {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+     
+        System.out.print("Enter your score: ");
+        double score = scanner.nextDouble();
+        
+    
+        if (score >= 60) {
+            System.out.println("You are pass");
+        } else {
+            System.out.println("You are not pass");
+        }
+        
+        scanner.close();
+    }
+}
+
