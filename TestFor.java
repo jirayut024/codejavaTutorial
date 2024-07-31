6654077b59efc1282e42d8c9dcf6506be5fd172f
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-07-31 15:42:23 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,11 @@
+public class TestFor {
+    public static void main(String[] args) {
+        int number = 2; 
+
+        System.out.println("สูตรคูณแม่ " + number + ":");
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
+
