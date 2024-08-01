6f397a68318a7de7fbadc4c72cfe5376756e331c
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-01 10:03:57 +0700

    Create result1to100.java

---
+++ result1to100.java
@@ -0,0 +1,11 @@
+public class result1to100 {
+    public static void main(String[] args) {
+        int non = 0;
+        int ball = 1;
+        while (ball <= 100){
+           non += ball;
+            ball++ ;
+    }
+    System.out.println("ผลลับ 1  ถึง 100 :"+non);
+}
+}
