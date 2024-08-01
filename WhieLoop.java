bbf23d932335bb4b990309fbf23d8418c33cf9af
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-01 10:04:02 +0700

    Create WhieLoop.java

---
+++ WhieLoop.java
@@ -0,0 +1,12 @@
+public class WhieLoop {
+    public static void main(String[] args) {
+       /* for(int i = 1; i <= 10; i++){
+            System.out.println("reslut in forloop : "+i);
+        } */
+        int iWhie = 1;
+        while (iWhie <= 5){
+            System.out.println("hllo");
+            iWhie++ ;
+        }
+    }
+}
