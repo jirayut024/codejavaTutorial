d2a741850510b5bcfeac6d41e164d9b01216d4a6
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-01 11:23:31 +0700

    Update result1to100.java

--- result1to100.java
+++ result1to100.java
@@ -3,6 +3,7 @@ public class result1to100 {
         int non = 0, ball = 1;
         while (ball <= 100){
            non += ball;
+           System.out.println("ball : " +ball+ "non : "+non);
             ball++ ;
     }
     System.out.println("ผลลับ 1  ถึง 100 :"+non);
