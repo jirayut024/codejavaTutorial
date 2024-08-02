71ec14951bb022c2c98b1259a36ed57f2669a949
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-02 10:14:22 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+        displayMessage();
+}
+}
+public static void displayMessage(){
+    System.out.println("จิรยุทธ ศิริวัฒน์");
+}
+}
