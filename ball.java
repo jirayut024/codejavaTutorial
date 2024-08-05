10ddc379af30fa773825ee7589fcaaf51f79f306
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-05 15:28:02 +0700

    Create ball.java

---
+++ ball.java
@@ -0,0 +1,11 @@
+public class ball {
+    public static void main(String[] args) {
+       
+        String[] strings = {"สวัสดี", "โลก", "นี่คือ", "การทดสอบ"};
+        
+      
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
