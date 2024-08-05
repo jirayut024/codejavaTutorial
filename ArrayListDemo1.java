11df15defa6256bc80ddf07a112fcea8b52c9984
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-05 15:27:59 +0700

    Create ArrayListDemo1.java

---
+++ ArrayListDemo1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class ArrayListDemo1 {
+    public static void main(String[] args) {
+        ArrayList nameList = new ArrayList();
+        nameList.add("ball");
+        nameList.add("Java");
+        nameList.add("Micro");
+        System.out.println("The arrayList has "+nameList.size()+" objects stored in it.");
+        for(int index = 0; index < nameList.size(); index++){
+            System.out.println(nameList.get(index));
+         
+        }
+    }
+}
