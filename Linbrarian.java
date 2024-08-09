f71f5c6eaad3ff7b9425c771919637a92074e11b
jirayut024 <152240644+jirayut024@users.noreply.github.com>
2024-08-09 11:44:20 +0700

    Create Linbrarian.java

---
+++ Linbrarian.java
@@ -0,0 +1,19 @@
+public class Linbrarian {
+    public static void main(String[] args) {
+        Student1 st1;
+        LibraryCard card1, card2;
+        st1 = new Student1();
+        st1.setName("Jon Java");
+        st1.setemail("jj@gmail.com");
+        card1 = new LibraryCard();
+        card1.setOwnerName(st1);
+        card1.checkout(3);
+        card2= new LibraryCard();
+        card2.setOwnerName(st1);
+
+        System.out.println("Card1 Info: ");
+        System.out.println(card1.toString()+"\n");
+        System.out.println("Card2 Info: ");
+        System.out.println(card2.toString()+"\n");
+    }
+}
